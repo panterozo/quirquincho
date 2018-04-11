637ee15953c6439329fc02d6538f7fd03b475540
Camilo Castro <camilo@ninjas.cl>
2018-04-11 11:10:40 -0300

    modified command structure

---
+++ bot/commands/help/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Implements all the command related classes
+"""
+
+import bots.commands.help.controller
+import bots.commands.help.model
+import bots.commands.help.requester
+import bots.commands.help.validator
+import bots.commands.help.view
+
+name = 'help'
